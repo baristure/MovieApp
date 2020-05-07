16a3d930c73e7eee2ca69f2405941852c792f760
Barış Türe <baristure@hotmail.com>
2020-05-07 23:59:21 +0300

    header component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,25 @@
 import React, { Component }  from 'react';
 import './App.css';
-import {Container,Image,Menu,Visibility,} from 'semantic-ui-react'
+import {Container} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
-import { menuStyle, fixedMenuStyle } from './helpers/styleHelper';
+
 import MoviesPage from './components/pages/MoviesPage';
 import Footer from './components/Footer'
+import Header from './components/Header'
 
-import { Link,Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 
 class App extends Component {
 
-	state = {
-		menuFixed: null,
-		overlayFixed: false,
-	};
-
-	stickTopMenu = () => this.setState({ menuFixed: true });
-	unStickTopMenu = () => this.setState({ menuFixed: null });
 
   render() {
-		const { menuFixed } = this.state;
+
 
 		return (
       <div className="App">
-				<Visibility
-					onBottomPassed={this.stickTopMenu}
-					onBottomVisible={this.unStickTopMenu}
-					once={false}
-				>
-					<Menu
-						borderless
-						fixed={menuFixed && 'top'}
-						style={menuFixed ? fixedMenuStyle : menuStyle}
-					>
-						<Container text>
-							<Menu.Item>
-								<Image size='mini' src='https://react.semantic-ui.com/logo.png' />
-							</Menu.Item>
-							<Menu.Item header>Movieapp</Menu.Item>
-							<Menu.Item as={Link} to="/movies">
-								Movies
-              </Menu.Item>
-							<Menu.Item as='a'>Add New</Menu.Item>
-						</Container>
-					</Menu>
-				</Visibility>
 
+      <Header/>
 				<Container text>
 					<Route path='/movies' component={MoviesPage}></Route>
 
