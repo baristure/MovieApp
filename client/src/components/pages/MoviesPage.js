96c599a1f0956b178ed229e03254bb862b17c364
Barış Türe <baristure@hotmail.com>
2020-05-07 23:34:05 +0300

    Layout added

--- client/src/components/pages/MoviesPage.js
+++ client/src/components/pages/MoviesPage.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class MoviesPage extends Component {
+import MoviesList from '../MoviesList';
+
+class MoviesPage extends Component {
+    static propTypes={
+        movies: PropTypes.array.isRequired
+    };
+
     render() {
         return (
             <div>
                 <h2>Movies Page</h2>
+                <MoviesList movies={this.props.movies}/>
             </div>
         )
     }
 }
+
+const mapStateToProps = ({movies}) =>{
+    return  {
+        movies
+    }
+};
+
+
+export default connect(mapStateToProps)(MoviesPage);
